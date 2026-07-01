182bed6ab9400acd0844a93ab542dc7f1eb443f2
annmarycyriac <annmarycyriac10@gmail.com>
2026-07-01 23:28:15 +0530

    Add files via upload

---
+++ TDD using JUnit5 and Mockito/Calculator/src/Calculator.java
@@ -0,0 +1,19 @@
+package com.cognizant.junitbasics;
+
+/**
+ * Exercise 1: Setting Up JUnit
+ * ------------------------------------------------------------------
+ * A simple class used to demonstrate that JUnit has been correctly
+ * added to the project and a test class can successfully find,
+ * compile against, and run against it.
+ */
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
