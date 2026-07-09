af260744b6b8f62c2e9dc6a5a2532eb20d9b0b8a
annmarycyriac <annmarycyriac10@gmail.com>
2026-07-09 18:02:16 +0530

    Update ExternalApi.java

--- Mockito exercises/mocking and stubbing/src/ExternalApi.java
+++ Mockito exercises/mocking and stubbing/src/ExternalApi.java
@@ -1,18 +1,7 @@
 package com.example;
 
-/**
- * Represents an external API dependency that MyService relies on.
- * In a real application this might make an HTTP call, hit a database, etc.
- */
 public interface ExternalApi {
 
-    /**
-     * Fetches some data with no arguments.
-     */
     String getData();
-
-    /**
-     * Fetches data for a specific id.
-     */
     String getDataById(String id);
 }
