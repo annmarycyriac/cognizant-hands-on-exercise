9e1022a3ea4e72d761d108055310544c44587b0c
annmarycyriac <annmarycyriac10@gmail.com>
2026-07-09 17:59:15 +0530

    Update Document.java

--- Design Patterns and Principles/FactoryMethodPatternExample/src/Document.java
+++ Design Patterns and Principles/FactoryMethodPatternExample/src/Document.java
@@ -1,9 +1,5 @@
 package com.example.factory;
 
-/**
- * Common interface implemented by every kind of document
- * our document-management system can produce.
- */
 public interface Document {
     void open();
     void save();
