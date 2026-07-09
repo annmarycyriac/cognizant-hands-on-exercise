f061f1ebde7eb09ea0c3a005d77e1ca2fa76dbe6
annmarycyriac <annmarycyriac10@gmail.com>
2026-07-09 18:14:32 +0530

    Update CalculatorSetupTest.java

--- TDD using JUnit5 and Mockito/Calculator/src/CalculatorSetupTest.java
+++ TDD using JUnit5 and Mockito/Calculator/src/CalculatorSetupTest.java
@@ -3,22 +3,6 @@ package com.cognizant.junitbasics;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
-/**
- * Exercise 1: Setting Up JUnit
- * ------------------------------------------------------------------
- * This test class exists purely to confirm that the JUnit dependency
- * has been added correctly to the project (via Maven's pom.xml, see
- * pom.xml in this folder) and that a test class can be created,
- * compiled, and executed successfully.
- *
- * Maven dependency used (as specified in the exercise):
- *   <dependency>
- *       <groupId>junit</groupId>
- *       <artifactId>junit</artifactId>
- *       <version>4.13.2</version>
- *       <scope>test</scope>
- *   </dependency>
- */
 public class CalculatorSetupTest {
 
     @Test
